aab880b0ca39aba15b2e479bc0c1f86b58adfa4f
Nagarjun Bagalore <bsnagarjun90@gmail.com>
2023-04-19 16:26:26 +0200

    added gmail pass

--- dj/settings.py
+++ dj/settings.py
@@ -195,7 +195,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_POST = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'prabhu.shanky89@gmail.com'
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_PASSWORD = 'rqvrczchhomruyjx'
 
 
 
